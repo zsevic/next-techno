5d8eb2082427131157c1caabcb16d98bce162ee0
zsevic <zeljkosevic95@gmail.com>
2017-02-27 23:16:14 +0100

    fixed updating database

--- app/routes.js
+++ app/routes.js
@@ -14,7 +14,7 @@ module.exports=function(app,passport){
 		var Event=require("./models/event");
 		var Page=require("./models/page");
 		var lastUpdate=Date.now()-(new Date(user.facebook.lastUpdated));
-		if(lastUpdate<3600000){
+		if(lastUpdate>3600000){
 			User.update({'facebook.id':user.facebook.id},{$set:{
 				'facebook.lastUpdated':new Date()
 			}}).then(function(){
@@ -32,20 +32,9 @@ module.exports=function(app,passport){
 				});
 			});
 		}else{
-			var arr=[];
-			var ids=[];
 			Event.find({},function(err,docs){
-				for(var i=0;i<docs.length;i++){
-					if(ids.indexOf(docs[i].id)>-1){
-						continue;
-					}else{
-						ids.push(res[i].id);
-						var temp={event:res[i].name,place:res[i].place.name,start_time:res[i].start_time};
-						arr.push(temp);
-					}
-				}
-				var result=arr.sort(function(a,b){
-						return (new Date(a.start_time))-(new Date(b.start_time));
+				var result=docs.sort(function(a,b){
+					return (new Date(a.start_time))-(new Date(b.start_time));
 				});
 				res.render('events.ejs',{user:req.user,result:result});
 			});
