96e033ca349d0fe74579808d1f182274596d4b12
zsevic <zsevic95@gmail.com>
2018-02-08 01:15:45 +0100

    :sparkles: set multiple events

--- nextTechno.js
+++ nextTechno.js
@@ -13,29 +13,48 @@ function registerEvent (ids, res, arr) {
   arr.push(temp)
   let newEvent = new Event(temp)
   newEvent.save()
+  return 0
+}
+
+function checkForEvents (ids, res, arr, name) {
+  res.name = res.name ? res.name : name
+  let today = Date.now()
+  let startTime = Date.parse(res.start_time)
+  if (ids.indexOf(res.id) > -1) {
+    return 1 // continue
+  } else if (today > startTime) {
+    if (res.end_time && today < Date.parse(res.end_time)) {
+      registerEvent(ids, res, arr)
+    } else {
+      return -1 // break
+    }
+  } else {
+    return registerEvent(ids, res, arr) // 200
+  }
 }
 
 module.exports = (pages, token, done) => {
   const arr = []
   const ids = []
-  let today = Date.now()
   let giveMeData = (page, cb) => {
     let access = `https://graph.facebook.com/v2.9/${page.id}/events?access_token=${token}`
     request(access, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         const res = JSON.parse(body).data
         for (let i = 0; i < res.length; i++) {
-          let startTime = Date.parse(res[i].start_time)
-          if (ids.indexOf(res[i].id) > -1) {
-            continue
-          } else if (today > startTime) {
-            if (res[i].end_time && today < Date.parse(res[i].end_time)) {
-              registerEvent(ids, res[i], arr)
-            } else {
+          if (res[i].event_times) {
+            res[i].event_times.forEach(event => {
+              event.name = res[i].name
+              event.place = res[i].place
+              checkForEvents(ids, event, arr)
+            })
+          } else {
+            let result = checkForEvents(ids, res[i], arr)
+            if (result === 1) {
+              continue
+            } else if (result === -1) {
               break
             }
-          } else {
-            registerEvent(ids, res[i], arr)
           }
         }
         return cb(null)
